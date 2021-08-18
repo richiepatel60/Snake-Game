ff5aefb13fbcdbae497c3cf6fb537f625b8c2bcc
richie-AI <79308498+richie-AI@users.noreply.github.com>
2021-08-18 12:50:34 +0530

    Add files via upload

---
+++ day- 20,21 - snake game/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALIGNMENT = 'center'
+FONT = ("Courier", 15, "normal")
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        self.speed("fastest")
+        self.penup()
+        self.hideturtle()
+        self.goto(x=0, y=270)
+        self.pencolor("white")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
---
+++ day- 20,21 - snake game/snake.py
@@ -0,0 +1,63 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, positions):
+        new_segment = Turtle('square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(positions)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x=new_x, y=new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        current_heading = self.head.heading()
+        # if current_heading != 270:
+        #     self.segments[0].setheading(90)
+        if current_heading == DOWN:
+            return
+        self.segments[0].setheading(UP)
+
+    def down(self):
+        current_heading = self.head.heading()
+        if current_heading == UP:
+            return
+        self.segments[0].setheading(DOWN)
+
+    def left(self):
+        current_heading = self.head.heading()
+        if current_heading == RIGHT:
+            return
+        self.segments[0].setheading(LEFT)
+
+    def right(self):
+        current_heading = self.head.heading()
+        if current_heading == LEFT:
+            return
+        self.segments[0].setheading(RIGHT)
